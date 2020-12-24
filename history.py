2c95631b2b10ef56c439435eed1f07a9739e788e
gabyshev_at <gabyshev_at@albank.ru>
2020-12-25 02:13:10 +0900

    crawler add tree search and output to CSV

---
+++ history.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Dec 24 22:35:00 2020)---
+pip install beautifulsoup4
+pip install lxml
+runfile('C:/Users/Император Борща/.spyder-py3/temp.py', wdir='C:/Users/Император Борща/.spyder-py3')
+pip install selenium
+runfile('C:/Users/Император Борща/.spyder-py3/temp.py', wdir='C:/Users/Император Борща/.spyder-py3')
+runcell(0, 'C:/Users/Император Борща/.spyder-py3/temp.py')
+runfile('C:/Users/Император Борща/.spyder-py3/temp.py', wdir='C:/Users/Император Борща/.spyder-py3')
+runcell(0, 'C:/Users/Император Борща/.spyder-py3/temp.py')
+git init
+git add .
---
+++ main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests as req
+from selenium import webdriver
+
+session = req.Session()
+data = {"login_username":"user", "login_password":"password"}
+url = "http://github.com/login.php"
+user_agent = ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
+              'Gecko/20100101 Firefox/50.0')
+url_search = "https://github.com/search?q=vue"
+ssl=""
+response = session.post(url, data=data)
+href_array=[]
+
+def tree(href_array,ssl):
+    for row in table.findAll('tr')[1:]:
+    col = row.findAll('td')
+    response = session.post(url, data=ssl)
+    d = {    
+            'tag' : col[0].getText(),
+            'component': col[1].getText(),
+            'name' : col[2].getText(),
+            'framework' : col[3].getText(),
+        }
+
+    data.append(d)
+    return data
+
+for page in range(5):
+    url_page = "https://github.com/search?p={}&q=vue&type=Repositories".format(page)
+    response = req.get(url_page, timeout=(25, 5), headers={'User-Agent':user_agent}).content
+    soup = BeautifulSoup(response, "lxml")
+    soup.prettify()
+    for link in soup.find_all("div", { "class":"f4 text-normal"}):
+        href = [item['href'] for item in link.select('[href]')]
+        href_array.append(href[0])
+        for repository_access in range(len(href_array)):
+            url_repository = "https://github.com/{}".format(repository_access)
+            response_repository = req.get(url_repository, timeout=(25, 5), headers={'User-Agent':user_agent}).content
+            repository = BeautifulSoup(response_repository, "lxml")
+            repository.prettify()
+            for link in repository.find_all("a", { "class":"js-navigation-open link-gray-dark"}):
+                href = [item['href'] for item in link.select('[href]')]
+                href_array.append(href[0])
+                answer = tree(href_array)
+
+order=['tag', 'component', 'name', 'framework']                
+with open('output.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=order)
+    writer.writeheader()
+    writer.writerows(answer)
